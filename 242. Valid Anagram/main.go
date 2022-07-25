28ef6e62a349963d320c5ace5ef2e018ec7ffac0
tf838499 <tf88981213@gmail.com>
2022-07-25 15:08:44 +0800

    ✨ feat(242. Valid Anagram):
    complete 242. Valid Anagram

---
+++ 242. Valid Anagram/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	// "strconv"
+)
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	m := make(map[string]int)
+	n := make(map[string]int)
+	a := []rune(s)
+	b := []rune(t)
+	// str := strings.Split("HELLO", "")
+	for i := 0; i < len(s); i++ {
+		m[string(a[i])] = m[string(a[i])] + 1
+		n[string(b[i])] = n[string(b[i])] + 1
+	}
+	for i := 0; i < len(a); i++ {
+		if m[string(a[i])] != n[string(a[i])] {
+			return false
+		}
+	}
+	return true
+}
+
+// func isAnagram(s string, t string) bool {
+// 	if len(s) != len(t) {
+// 		return false
+// 	}
+
+// 	mS := make(map[rune]int)
+// 	mT := make(map[rune]int)
+
+// 	for _, v := range s {
+// 		mS[v]++
+// 	}
+
+// 	for _, v := range t {
+// 		mT[v]++
+// 	}
+
+// 	for k, v := range mS {
+// 		if v != mT[k] {
+// 			return false
+// 		}
+// 	}
+
+// 	return true
+// }
+
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+
+ypically using all the original letters exactly once.
+
+anagram : 同字母異序字,重組字,變位字
+
+Input: s = "anagram", t = "nagaram"
+Input: s = "rat", t = "car"
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+*/
+func main() {
+	// s := "anagram"
+	// t := "nagaram"
+
+	s := "rat"
+	t := "car"
+	fmt.Println(len(s))
+	fmt.Println(len(t))
+	fmt.Println(isAnagram(s, t))
+
+}
--- 707. Design Linked List/main.go
+++ 707. Design Linked List/main.go
@@ -1,116 +1,236 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"strconv"
+// )
+
+// /**
+//  * Definition for singly-linked list.
+//  * type ListNode struct {
+//  *     Val int
+//  *     Next *ListNode
+//  * }
+//  */
+
+// type MyLinkedList struct {
+// 	head *ListNode1
+// 	// head ListNode // 會變初始化建立一個val = 0 Next = nil
+
+// }
+// type ListNode1 struct {
+// 	Val  int
+// 	Next *ListNode1
+// }
+
+// func Constructor() MyLinkedList {
+// 	objLinkedList := new(MyLinkedList)
+// 	return *objLinkedList
+// }
+
+// func (this *MyLinkedList) Get(index int) int {
+// 	tmp := this.head
+// 	counter := 0
+// 	for tmp != nil {
+// 		if index == counter {
+// 			return tmp.Val
+// 		}
+// 		tmp = tmp.Next
+// 		counter++
+// 	}
+// 	return -1
+// }
+
+// func (this *MyLinkedList) AddAtHead(val int) {
+// 	addHead := ListNode1{Val: val}
+// 	addHead.Next = this.head
+// 	this.head = &addHead
+// }
+
+// func (this *MyLinkedList) AddAtTail(val int) {
+// 	tmp := this.head
+// 	if this.head == nil {
+// 		this.AddAtHead(val)
+// 	} else {
+// 		for this.head.Next != nil {
+// 			this.head = this.head.Next
+// 		}
+// 		this.head.Next = &ListNode1{Val: val}
+// 		this.head = tmp
+// 	}
+
+// }
+
+// func (this *MyLinkedList) AddAtIndex(index int, val int) {
+
+// 	tmp := this.head
+// 	counter := 0
+
+// 	if index == 0 && tmp == nil {
+// 		this.head = &ListNode1{Val: val, Next: nil}
+// 		return
+// 	} else if index == 0 {
+// 		this.head = &ListNode1{Val: val, Next: tmp}
+// 		return
+// 	}
+// 	for index != counter {
+// 		counter++
+// 		if index == counter {
+// 			if tmp == nil {
+// 				return
+// 			} else if tmp.Next != nil {
+// 				tmp.Next = &ListNode1{Val: val, Next: tmp.Next}
+// 			} else {
+// 				tmp.Next = &ListNode1{Val: val}
+// 			}
+// 			return
+// 		}
+// 		tmp = tmp.Next
+// 	}
+// 	return
+// }
+
+// func (this *MyLinkedList) DeleteAtIndex(index int) {
+// 	tmp := this.head
+// 	counter := 0
+// 	var prev *ListNode1
+// 	for tmp != nil {
+// 		if index == counter {
+// 			if prev == nil {
+// 				this.head = tmp.Next
+// 				break
+// 			}
+// 			if prev.Next.Next == nil {
+// 				prev.Next = nil
+// 			} else {
+// 				prev.Next = prev.Next.Next
+// 			}
+// 			break
+// 		}
+// 		prev = tmp
+// 		tmp = tmp.Next
+// 		counter++
+// 	}
+// }
+
+// /**
+//  * Your MyLinkedList object will be instantiated and called as such:
+//  * obj := Constructor();
+//  * param_1 := obj.Get(index);
+//  * obj.AddAtHead(val);
+//  * obj.AddAtTail(val);
+//  * obj.AddAtIndex(index,val);
+//  * obj.DeleteAtIndex(index);
+//  */
+// func outputMessage(head *ListNode1, title string) {
+// 	foo := title
+// 	for head != nil {
+// 		foo = foo + strconv.Itoa(head.Val) + " "
+// 		head = head.Next
+// 	}
+// 	fmt.Print(foo + "\n")
+// }
+// func main() {
+
+// 	obj := Constructor()
+// 	// obj.AddAtHead(1)
+// 	// obj.AddAtTail(3)
+// 	obj.AddAtIndex(0, 1)
+// 	obj.AddAtIndex(1, 2)
+// 	// obj.AddAtIndex(1, 3)
+// 	// obj.Get(1)
+// 	// obj.DeleteAtIndex(0)
+// 	obj.Get(0)
+
+// 	// outputMessage(obj, "input : ")
+
+// }
+
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+import "fmt"
 
 type MyLinkedList struct {
-	head *ListNode1
-	// head ListNode // 會變初始化建立一個val = 0 Next = nil
-
-}
-type ListNode1 struct {
 	Val  int
-	Next *ListNode1
+	Next *MyLinkedList
 }
+
+// type MyLinkedList struct {
+// 	head *ListNode
+// }
+
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
 
 func Constructor() MyLinkedList {
-	objLinkedList := new(MyLinkedList)
-	return *objLinkedList
+	obj := MyLinkedList{
+		Val:  1000,
+		Next: nil,
+	}
+	// obj := new(MyLinkedList)
+	return obj
 }
 
 func (this *MyLinkedList) Get(index int) int {
-	tmp := this.head
-	counter := 0
-	for tmp != nil {
-		if index == counter {
-			return tmp.Val
+	loop := 0
+	// head := this.head
+	head := this
+	if head != nil {
+		for head.Next != nil {
+			if loop == index {
+				return head.Val
+			}
+			head = head.Next
+			loop = loop + 1
 		}
-		tmp = tmp.Next
-		counter++
+		return -1
+	} else {
+		return -1
 	}
-	return -1
 }
 
 func (this *MyLinkedList) AddAtHead(val int) {
-	addHead := ListNode1{Val: val}
-	addHead.Next = this.head
-	this.head = &addHead
+	// obj1 := this.head
+	obj1 := this
+	// obj := ListNode{
+	// 	Val:  val,
+	// 	Next: obj1,
+	// }
+	obj := &MyLinkedList{
+		Val:  val,
+		Next: obj1,
+	}
+
+	fmt.Println("47", &obj)
+	// this.head = &obj
+	this = obj
+	fmt.Println("48", &this)
+
 }
 
 func (this *MyLinkedList) AddAtTail(val int) {
-	tmp := this.head
-	if this.head == nil {
-		this.AddAtHead(val)
-	} else {
-		for this.head.Next != nil {
-			this.head = this.head.Next
-		}
-		this.head.Next = &ListNode1{Val: val}
-		this.head = tmp
-	}
 
 }
 
 func (this *MyLinkedList) AddAtIndex(index int, val int) {
 
-	tmp := this.head
-	counter := 0
-
-	if index == 0 && tmp == nil {
-		this.head = &ListNode1{Val: val, Next: nil}
-		return
-	} else if index == 0 {
-		this.head = &ListNode1{Val: val, Next: tmp}
-		return
-	}
-	for index != counter {
-		counter++
-		if index == counter {
-			if tmp == nil {
-				return
-			} else if tmp.Next != nil {
-				tmp.Next = &ListNode1{Val: val, Next: tmp.Next}
-			} else {
-				tmp.Next = &ListNode1{Val: val}
-			}
-			return
-		}
-		tmp = tmp.Next
-	}
-	return
 }
 
 func (this *MyLinkedList) DeleteAtIndex(index int) {
-	tmp := this.head
-	counter := 0
-	var prev *ListNode1
-	for tmp != nil {
-		if index == counter {
-			if prev == nil {
-				this.head = tmp.Next
-				break
-			}
-			if prev.Next.Next == nil {
-				prev.Next = nil
-			} else {
-				prev.Next = prev.Next.Next
-			}
-			break
-		}
-		prev = tmp
-		tmp = tmp.Next
-		counter++
-	}
+
+}
+
+func main() {
+	obj := Constructor()
+	fmt.Println("79", *(&obj))
+	fmt.Println(obj.Get(2))
+
+	obj.AddAtHead(10)
+
+	fmt.Println(obj)
+
 }
 
 /**
@@ -122,26 +242,3 @@ func (this *MyLinkedList) DeleteAtIndex(index int) {
  * obj.AddAtIndex(index,val);
  * obj.DeleteAtIndex(index);
  */
-func outputMessage(head *ListNode1, title string) {
-	foo := title
-	for head != nil {
-		foo = foo + strconv.Itoa(head.Val) + " "
-		head = head.Next
-	}
-	fmt.Print(foo + "\n")
-}
-func main() {
-
-	obj := Constructor()
-	// obj.AddAtHead(1)
-	// obj.AddAtTail(3)
-	obj.AddAtIndex(0, 1)
-	obj.AddAtIndex(1, 2)
-	// obj.AddAtIndex(1, 3)
-	// obj.Get(1)
-	// obj.DeleteAtIndex(0)
-	obj.Get(0)
-
-	// outputMessage(obj, "input : ")
-
-}
