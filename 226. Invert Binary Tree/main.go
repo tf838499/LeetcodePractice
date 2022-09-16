6161a6d87291813987804c703d394249c2473025
tf838499 <tf88981213@gmail.com>
2022-09-16 11:46:41 +0800

    ✨ feat(binary tree): invert binary
    Complete

--- 226. Invert Binary Tree/main.go
+++ 226. Invert Binary Tree/main.go
@@ -21,45 +21,64 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func largestValues(root *TreeNode) (ans []int) {
-	stack := []*TreeNode{root}
-	const INT_MAX = int(^uint(0) >> 1)
+func invertTree(root *TreeNode) *TreeNode {
 	if root == nil {
-		return
+		return root
 	}
-	for len(stack) != 0 {
-		maxVal := ^INT_MAX
-		for _, i := range stack {
-			stack = stack[1:]
-			if i.Left != nil {
-				stack = append(stack, i.Left)
-			}
-			if i.Right != nil {
-				stack = append(stack, i.Right)
-			}
-			if i.Val > maxVal {
-				maxVal = i.Val
-			}
-		}
-		ans = append(ans, maxVal)
-	}
-	return
+	root.Left, root.Right = root.Right, root.Left
+	// swap(root.Left, root.Right)
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
 }
 
-func main() {
+// func largestValues(root *TreeNode) (ans []int) {
+// 	stack := []*TreeNode{root}
+// 	const INT_MAX = int(^uint(0) >> 1)
+// 	if root == nil {
+// 		return
+// 	}
+// 	for len(stack) != 0 {
+// 		maxVal := ^INT_MAX
+// 		for _, i := range stack {
+// 			stack = stack[1:]
+// 			if i.Left != nil {
+// 				stack = append(stack, i.Left)
+// 			}
+// 			if i.Right != nil {
+// 				stack = append(stack, i.Right)
+// 			}
+// 			if i.Val > maxVal {
+// 				maxVal = i.Val
+// 			}
+// 		}
+// 		ans = append(ans, maxVal)
+// 	}
+// 	return
+// }
 
-	// var Node4 *TreeNode = &TreeNode{
-	// 	Val: 4.0,
-	// }
+func main() {
+	var Node7 *TreeNode = &TreeNode{
+		Val: 7.0,
+	}
+	var Node6 *TreeNode = &TreeNode{
+		Val: 6.0,
+	}
+	var Node5 *TreeNode = &TreeNode{
+		Val: 5.0,
+	}
+	var Node4 *TreeNode = &TreeNode{
+		Val: 4.0,
+	}
 	var Node3 *TreeNode = &TreeNode{
-		Val: 3.0,
+		Val: 3.0, Left: Node6, Right: Node7,
 	}
 	var Node2 *TreeNode = &TreeNode{
-		Val: -1.0,
+		Val: 2.0, Left: Node4, Right: Node5,
 	}
 	var Node1 *TreeNode = &TreeNode{
 		Val: 1.0, Left: Node2, Right: Node3,
 	}
 
-	largestValues(Node1)
+	invertTree(Node1)
 }
